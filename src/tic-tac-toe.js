ff6f6c067c7d912294ce48ccc422b915e0a6ad7c
Sergey Vnukov <skivun@hotmail.com>
2019-10-07 00:41:09 +0300

    task: tic-tac-toe

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,54 @@
 class TicTacToe {
     constructor() {
-
+        this.CurrentPlayerSymbol = 'x';
+        this.field = [[null, null , null], [null, null, null], [null, null, null]];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.CurrentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(!this.field[rowIndex][columnIndex]){
+            this.field[rowIndex][columnIndex] = this.CurrentPlayerSymbol;
+            this.CurrentPlayerSymbol = this.CurrentPlayerSymbol == 'x' ? 'o' : 'x';
+        }
     }
 
     isFinished() {
-
+        return this.isDraw() || Boolean(this.getWinner());
     }
 
     getWinner() {
+        if( this.field[0][0] == this.field[0][1] && this.field[0][1] == this.field[0][2]) return this.field[0][0];
+        if( this.field[1][0] == this.field[1][1] && this.field[1][1] == this.field[1][2]) return this.field[1][0];
+        if( this.field[2][0] == this.field[2][1] && this.field[2][1] == this.field[2][2]) return this.field[2][0];
+        
+        if( this.field[0][0] == this.field[1][0] && this.field[1][0] == this.field[2][0]) return this.field[0][0];
+        if( this.field[0][1] == this.field[1][1] && this.field[1][1] == this.field[2][1]) return this.field[0][1];
+        if( this.field[0][2] == this.field[1][2] && this.field[1][2] == this.field[2][2]) return this.field[0][2];
 
+        if( this.field[0][0] == this.field[1][1] && this.field[1][1] == this.field[2][2]) return this.field[0][0];
+        if( this.field[0][2] == this.field[1][1] && this.field[1][1] == this.field[2][0]) return this.field[2][0];
+    
+        return null;
     }
 
     noMoreTurns() {
-
+        for(let i = 0; i<3; i++){
+            for(let j = 0; j<3; j++){
+                if(!this.field[i][j]) return false;
+            }
+        }
+    return true;
     }
 
     isDraw() {
-
+        return this.noMoreTurns() && !this.getWinner();
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
